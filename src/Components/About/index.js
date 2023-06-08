4bd891fba0a89e66f90bd29c3acf876efee02432
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-08 17:57:45 -0300

    first commit

---
+++ src/Components/About/index.js
@@ -0,0 +1,36 @@
+import styles from './About.css'
+
+export default function About() {
+    return (
+        <section>
+
+            <div className='div-apresentacao d-flex flex-column justify-content-center align-items-center' id='about'>
+                <p className='p-apresentacao text-center'>
+                    Olá! Sou Iasmin Cursino, nutricionista clínica e esportiva comprometida em fornecer soluções nutricionais além da dieta convencional. Meu lema é "soluções nutricionais além da dieta".
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Como nutricionista, acredito que uma alimentação equilibrada e personalizada é fundamental para a saúde e o bem-estar. No entanto, entendo que as necessidades nutricionais de cada indivíduo são únicas e vão além do simples cálculo de calorias e macronutrientes.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Meu objetivo é oferecer soluções nutricionais abrangentes, considerando não apenas a dieta, mas também outros fatores importantes, como estilo de vida, preferências alimentares, condições de saúde e metas individuais. Com base nessas informações, trabalho com meus clientes para desenvolver planos alimentares adaptados às suas necessidades específicas.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Ofereço atendimentos online para todas as regiões, o que significa que posso ajudar pessoas em todo o mundo a alcançarem seus objetivos nutricionais, independentemente da localização geográfica. Através de consultas virtuais, posso fornecer orientação nutricional personalizada e acompanhamento contínuo para ajudar meus clientes a alcançarem seus objetivos de saúde.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Além disso, disponho de consultórios físicos em Volta Redonda - RJ, onde posso realizar atendimentos presenciais para aqueles que preferem essa modalidade. Nos meus consultórios, proporciono um ambiente acolhedor e personalizado, visando oferecer um atendimento atencioso e de qualidade.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Acredito que a nutrição vai além da dieta, envolvendo educação alimentar, mudanças de comportamento e apoio contínuo. Meu objetivo é capacitar meus clientes a adotarem escolhas alimentares saudáveis, equilibradas e sustentáveis, para que possam alcançar resultados duradouros.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Estou empenhada em oferecer um atendimento individualizado, baseado em evidências científicas atualizadas e em constante aprimoramento profissional. Busco estar sempre atualizada sobre as últimas pesquisas e tendências em nutrição, a fim de fornecer as melhores soluções nutricionais aos meus clientes.
+                </p>
+                <p className='p-apresentacao text-center'>
+                    Se você procura soluções nutricionais além da dieta convencional e deseja alcançar seus objetivos de saúde e bem-estar, estou pronta para ajudar. Vamos trabalhar juntos para desenvolver um plano alimentar personalizado e eficaz, que se adeque às suas necessidades individuais.
+                </p>
+
+            </div>
+        </section>
+    )
+}
